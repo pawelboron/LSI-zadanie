96e5518ae26fe958a67864235786e2ad76392451
Paweł Boroń <pawel.boron@externals.hilti.com>
2023-01-23 23:08:50 +0100

    NBP app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Table } from 'primeng/table';
+import { ApiService } from './api.service';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zadanie-rekrutacyjne';
+  ratesData:any = [];
+  ratesTable:any = [];
+  first: number = 0;
+
+  constructor(private api:ApiService, private themeService: ThemeService) {}
+
+
+  ngOnInit(): void {
+    this.api.getExchangeRates().subscribe((data) => {
+      this.ratesData = data;
+      this.ratesTable = this.ratesData[0].rates;
+      console.log(this.ratesTable);
+    });
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  changeTheme(theme: string){
+    this.themeService.switchTheme(theme);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng/table';
+import {PaginatorModule} from 'primeng/paginator';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TableModule,
+    PaginatorModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
